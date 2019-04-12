0aca12fe250429bf89d03fc80fe3b242f4cb639f
johnveloper <johnveloper@gmail.com>
2019-04-12 19:14:58 +0200

    Draw mouse position and frame number

--- index.js
+++ index.js
@@ -26,13 +26,25 @@ function handleMouseUp() {
 	mouse.down = false;
 }
 
+var frame = 0;
 function tick() {
 	requestAnimationFrame(tick);
+	frame += 1;
 	
 	// draw background - 100% tall and wide rectangle, every frame
 	// if left mouse button is down, the background will be red and otherwise green
 	context.fillStyle = mouse.down ? 'red' : 'green';
 	context.fillRect(0, 0, width, height);
+
+	// draw mouse position
+	context.fillStyle = 'black';
+	context.fillRect(mouse.x-2, mouse.y-2, 4, 4);
+	context.font = '16px verdana';
+	var mousePosString = '(' + mouse.x + ', ' + mouse.y + ')';
+	context.fillText(mousePosString, mouse.x + 24, mouse.y);
+
+	// draw frame number in top-left corner
+	context.fillText('Total frames: ' + frame, 16, 32);
 }
 
 window.addEventListener('mousedown', handleMouseDown);
